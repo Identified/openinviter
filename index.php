650141bd6f93cbce1389f68cb277bca2d238c1d6
Jason Feng <jason@identified.com>
2011-06-30 19:25:57 -0700

    Replaced index.php.

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
+include('openinviter.php');
 
+$email = _POST['email'];
+$password = _POST['password'];
+$provider = _POST['provider'];
+
+
+function errorcheck($invite)
+{
+  if ($inviter->getInternalError()) {
+    throw new Exception('Death!');
+  }
+}
+
+try {
+  if (!$email || !$password || !$provider) throw new Exception('Destruction!');
+  $inviter = new OpenInviter();
+  $inviter->startPlugin($provider);
+  errorcheck($inviter);
+  $inviter->login($email, $password);
+  errorcheck($inviter);
+  $contacts = $inviter->getMyContacts();
+  errorcheck($inviter);
+  if ($contacts === false) {
+    throw new Exception('Assmeat!');
+  }
+  echo $contacts;
+} 
+catch (Exception $e) {
+  echo "null";
+}
 ?>
