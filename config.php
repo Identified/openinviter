88f907bf9c34e966ddac07212838b1001920d177
root <root@ip-10-244-127-69.ec2.internal>
2011-07-01 02:03:08 +0000

    Initial commit of openinviter crap

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$openinviter_settings=array(
+'username'=>"turbofail", 'private_key'=>"89b0a559796f46dcb11da62b7ec1fb80", 'cookie_path'=>"/tmp", 'message_body'=>"You are invited to http://www.identified.com/", 'message_subject'=>" is inviting you to http://www.identified.com/", 'transport'=>"curl", 'local_debug'=>"on_error", 'remote_debug'=>"", 'hosted'=>"", 'proxies'=>array(),
+'stats'=>"", 'plugins_cache_time'=>"1800", 'plugins_cache_file'=>"oi_plugins.php", 'update_files'=>"1", 'stats_user'=>"", 'stats_password'=>"");
+?>
